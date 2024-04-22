6f0fcf67326d8ed9c7bb0ff092181c29fb7eb794
bryant-cloud <bryantcastellanos409@gmail.com>
2024-04-22 09:05:14 -0600

    cambio de back
    termindno bck primera fase

---
+++ back/index.js
@@ -0,0 +1,46 @@
+const express=require('express');
+const mysql=require('mysql');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+
+let server=express();
+
+
+server.use(cors({
+    origin:'*',
+    methos:'HEAD, GET, POT, DELETE, POST, PATH',
+}));
+
+
+server.use (bodyParser.urlencoded({extend:false}))
+server.use(bodyParser.json());
+
+
+//base de datos 
+const configdb={
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'',
+};
+
+const poolmysql=mysql.createPool(configdb);
+
+
+
+server.get("/",(req,resp)=>{
+    poolmysql.query(sql, function(err,result){
+        if(err) throw resp.end("Base de datos fallando");
+        res,poolmysql.json(result);
+    });
+});
+
+server.post("/",(req,resp)=>{
+    console.log(req);
+    let codigo=req.body.codigo;
+    let nombew=req.body.nombre;
+    let npasaporte=req.body.npasaporte;
+    let 
+
+
+    });
